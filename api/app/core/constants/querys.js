f4e69a7eabea2562ae4f70c33950ce88087a8d55
manu <manuel.villarna@gmail.com>
2025-05-09 00:56:10 +0100

    sacando 5 noticias en asideNews

--- api/app/core/constants/querys.js
+++ api/app/core/constants/querys.js
@@ -1,39 +1,51 @@
 const QUERIES = {
     ULTIMAS_NOTICIAS_PERIODICOS: `
-      SELECT
-    n.titulo,
-    DATE_FORMAT(n.fecha_publicacion, '%d-%m-%Y') AS fecha_publicacion,
-    p.nombre AS periodico_nombre,
-    n.url,
-    n.coeficiente
-    FROM
-        noticias n
-    JOIN
-        periodicos p ON p.id = n.periodico_id
-    JOIN (
-        SELECT
-            periodico_id,
-            MIN(fecha_scraping) AS min_fecha
-        FROM
-            noticias
-        WHERE DATE(fecha_scraping) = CURDATE()
-        GROUP BY
-            periodico_id
-    ) sub ON sub.periodico_id = n.periodico_id
-    AND n.fecha_scraping = sub.min_fecha
-    JOIN (
-        SELECT
-            periodico_id,
-            MIN(id) AS min_id
-        FROM
-            noticias
-        WHERE DATE(fecha_scraping) = CURDATE()
-        GROUP BY
-            periodico_id
-        ) sub2 ON sub2.periodico_id = n.periodico_id
-        AND n.id = sub2.min_id
-        ORDER BY
-            n.fecha_scraping DESC;
+      (
+            SELECT
+                n.titulo,
+                DATE_FORMAT(n.fecha_publicacion, '%d-%m-%Y') AS fecha_publicacion,
+                p.nombre AS periodico_nombre,
+                n.url,
+                n.coeficiente
+            FROM
+                noticias n
+            JOIN
+                periodicos p ON p.id = n.periodico_id
+            JOIN (
+                SELECT
+                    periodico_id,
+                    MIN(fecha_scraping) AS min_fecha
+                FROM
+                    noticias
+                WHERE DATE(fecha_scraping) = CURDATE()
+                GROUP BY periodico_id
+            ) sub ON sub.periodico_id = n.periodico_id AND n.fecha_scraping = sub.min_fecha
+            JOIN (
+                SELECT
+                    periodico_id,
+                    MIN(id) AS min_id
+                FROM
+                    noticias
+                WHERE DATE(fecha_scraping) = CURDATE()
+                GROUP BY periodico_id
+            ) sub2 ON sub2.periodico_id = n.periodico_id AND n.id = sub2.min_id
+            )
+            UNION
+            (
+            SELECT
+                n.titulo,
+                DATE_FORMAT(n.fecha_publicacion, '%d-%m-%Y'),
+                p.nombre AS periodico_nombre,
+                n.url,
+                n.coeficiente
+            FROM
+                noticias n
+            JOIN periodicos p ON p.id = n.periodico_id
+            WHERE DATE(n.fecha_scraping) = CURDATE()
+            ORDER BY RAND()
+            LIMIT 1
+            );
+
 
         `,
     TOPICOS_DIARIOS: `
